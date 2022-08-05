81b218e5f519a83f3e8bd73c5840302cf1b19c5e
jnk <57685812+while-zero@users.noreply.github.com>
2022-08-05 15:37:21 +0200

    yy

--- pj_dags/pj_databricks_submit_job.py
+++ pj_dags/pj_databricks_submit_job.py
@@ -1,29 +1,30 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
+from airflow.providers.databricks.operators.databricks import (
+    DatabricksSubmitRunOperator,
+)
 
 conf_json = {
     "notebook_task": {
         "notebook_path": "/Shared/some_db_notebook",
-        "notebook_params": {
+        "base_parameters": {
             "input": "xyz",
-        }
+        },
     },
     "existing_cluster_id": "0711-150317-tjta4wxy",
-    "run_name": "yyy"
+    "run_name": "yyy",
 }
 
 with DAG(
     dag_id="databricks_using_DatabricksSubmitRunOperator",
-    start_date=datetime(2022,8,1),
+    start_date=datetime(2022, 8, 1),
     schedule_interval="0 * * * *",
     catchup=False,
     tags=["databricks"],
 ) as dag:
     first_task_in_dag = DatabricksSubmitRunOperator(
-        task_id="task_1_db_submit_job",
-        json=conf_json
+        task_id="task_1_db_submit_job", json=conf_json
     )
 
     first_task_in_dag
